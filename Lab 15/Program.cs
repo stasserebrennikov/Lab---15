4bd7c328852fb38390fd71590aae0642a32f43da
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-01-23 23:58:07 +0300

    Добавьте файлы проекта.

---
+++ Lab 15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            arith.setStart(2);
+            arith.setStep(8);
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.ReadKey();
+            Console.WriteLine();
+
+            GeomProgression geom = new GeomProgression();
+            geom.setStart(1);
+            geom.setStep(3);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
